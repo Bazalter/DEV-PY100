b5961b46139460dfc01dc74c43dc3ac89af589a2
EgorZaikin <147304754+Bazalter@users.noreply.github.com>
2023-11-14 22:57:29 +0300

    Add files via upload

---
+++ Лабораторная работа №3/Поиск индекса элемента в списке товаров/task1.py
@@ -0,0 +1,15 @@
+def index_fruit (list_, fruit):
+    for i in list_:
+        if i == fruit:
+            return list_.index(i)
+
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = index_fruit (items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
